74c402d41d55c020ed110bf4c7e40f45f214ed5b
Pol Quintana <pol.quintana1@gmail.com>
2021-11-02 19:21:55 +0400

    Force Gif/Nuke executions

--- Sources/StreamChatUI/ChatChannelList/ChatChannelListVC.swift
+++ Sources/StreamChatUI/ChatChannelList/ChatChannelListVC.swift
@@ -2,7 +2,9 @@
 // Copyright © 2021 Stream.io Inc. All rights reserved.
 //
 
+import Nuke
 import StreamChat
+import SwiftyGif
 import UIKit
 
 /// A `UIViewController` subclass  that shows list of channels.
@@ -57,6 +59,14 @@ open class ChatChannelListVC: _ViewController,
 
     /// Used for mapping `ListChanges` to sets of `IndexPath` and verifying possible conflicts
     private let collectionUpdatesMapper = CollectionUpdatesMapper()
+
+    public static func testMeGif() {
+        SwiftyGifManager.defaultManager.clear()
+    }
+
+    public static func testMeNuke() {
+        let processor = ImageProcessors.Resize.resize(size: .zero)
+    }
 
     override open func setUp() {
         super.setUp()
